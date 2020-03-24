6de2cdf65f8bb22fe6ae2ef010e7bcb6190a0b14
binc0 <42704408+binc0@users.noreply.github.com>
2020-03-24 14:53:37 +0100

    Update Extension.php

--- Extension.php
+++ Extension.php
@@ -1,4 +1,4 @@
-<?php namespace Igniter\stripe-checkout;
+<?php namespace Binco\stripe-checkout;
 
 use System\Classes\BaseExtension;
 
@@ -7,7 +7,7 @@ class Extension extends BaseExtension
     public function registerPaymentGateways()
     {
         return [
-            'Igniter\stripe-checkout\Payments\Stripe' => [
+            'Binco\stripe-checkout\Payments\Stripe' => [
                 'code' => 'stripe',
                 'name' => 'lang:igniter.payregister::default.stripe.text_payment_title',
                 'description' => 'lang:igniter.payregister::default.stripe.text_payment_desc',
